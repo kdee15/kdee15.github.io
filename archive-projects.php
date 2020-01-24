69e4622005bd7f19674d53b9739d89928d55f135
kirk <kdee15@gmail.com>
2020-01-24 11:11:44 +0200

    files

--- archive-projects.php
+++ archive-projects.php
@@ -12,11 +12,51 @@ get_header(); ?>
 
     <section class="page archive-projects" id="mast">
 
-        <!-- C.2.1. INTRO ----------------------------------------------------------------------------------------- -->
 
-        <?php get_template_part( 'section-projects' ); ?>
+      <h2 class="a-block-title">{ projects }</h2>
 
-        <!-- C.2.1. End ------------------------------------------------------------------------------------------- -->
+      <div class="container">
+        <div class="row no-gutters">
+
+          <?php
+
+          $args=array(
+            'post_type' => 'projects',
+            'post_status' => 'publish',
+            'orderby' => 'meta_value date',
+            'order' => 'DESC'
+          );
+          $my_query = null;
+          $my_query = new WP_Query($args);
+
+          if( $my_query->have_posts() ) {
+            while ($my_query->have_posts()) : $my_query->the_post(); ?>
+
+              <article class="card blog-card col-12 col-md-6">
+                <a class="o-card hover-card" href="<?php the_permalink() ?>">
+                  <figure class="m-card-image">
+                    <img src="<?php the_field('cover_desk') ?>">
+                  </figure>
+                  <div class="m-card-body">
+                    <h3 class="a-card-header"><?php the_title(); ?></h3>
+                    <?php the_excerpt(); ?>
+                  </div>
+
+                </a>
+              </article>
+
+            <?php
+
+            endwhile;
+          }
+          wp_reset_query();  // Restore global post data stomped by the_post().
+          ?>
+
+        </div>
+
+        <a class="a-btn btn-blue" href="<?php echo esc_url( home_url( '/' ) ); ?>?page_id=26">VIEW MORE PROJECTS</a>
+
+      </div>
 
     </section>
     
